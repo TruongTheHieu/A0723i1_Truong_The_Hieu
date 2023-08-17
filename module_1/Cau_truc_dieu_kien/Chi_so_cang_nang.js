9c686a453fb06a1d376c8e4fb35cb2ffa5f938d8
TruongTheHieu <Thehieu191099@gmail.com>
2023-08-17 12:39:37 +0700

    nop bai

--- module_1/Cau_truc_dieu_kien/Chi_so_cang_nang.js
+++ module_1/Cau_truc_dieu_kien/Chi_so_cang_nang.js
@@ -5,7 +5,7 @@ function check() {
     let Bmi = weight/Math.pow(height,2); // Math.pow hàm lũy thừa
     if (Bmi < 18){
         document.getElementById("result").innerHTML="chỉ số Bmi : "+
-            Bmi+"<br><b>bạn ốm giống Hiền quá</b>"+"<img src='https://i.pinimg.com/originals/3b/97/63/3b97635b6bd4a5bfc436fbd3908f05e0.gif' width='100' height='100'>"
+            Bmi+"<br><b>ốm quá rồi</b>"+"<img src='https://i.pinimg.com/originals/3b/97/63/3b97635b6bd4a5bfc436fbd3908f05e0.gif' width='100' height='100'>"
     }else if (Bmi < 25.0) {
         document.getElementById("result").innerHTML="chỉ số Bmi : "+
             Bmi+"<br><b>bình thương nha</b>"+"<img src='https://img4.thuthuatphanmem.vn/uploads/2020/08/29/hinh-anh-dong-cute-dep_105546064.gif' width='100' height='100'>"
